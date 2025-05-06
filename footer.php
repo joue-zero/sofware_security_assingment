3f145265efefa30899eac80600e5c8ee3589d469
Youssef Elnaggar <elnagaryoussef864@gmail.com>
2025-05-06 15:39:20 +0300

    add report for gray test

--- footer.php
+++
@@ -1,6 +0,0 @@
-    </div>
-    <footer style="text-align: center; padding: 1rem; background-color: #333; color: white; margin-top: 2rem;">
-        <p>Vulnerable Bank Application - For Educational Purposes Only</p>
-    </footer>
-</body>
-</html> 
